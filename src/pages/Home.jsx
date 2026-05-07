5f57a94fc7fad2422ec2223ca5a1cc5d8df43389
Unish Maharjan <uniqueunish93@gmail.com>
2026-05-07 23:51:39 +0545

    bg added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,19 +7,33 @@ const Home = () => {
   return (
     <>
     <Header/>
-    <div className='flex flex-col py-20 px-8 gap-7 bg-contain shadow-inner shadow-blue-400' style={{
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1521587760476-6c12a4b040da')",
-    }}>
-    <div className='flex flex-col gap-5'>
-        <h1 className='text-5xl text-white font-[Poppins] font-semibold'>Discover Your Next Great Read</h1>
-        <h3 className='text-xl font-[Poppins] text-white w-[50%]'>Explore thousands of books across all genres. From timless classics to the latest bestsellers.</h3>
-    </div>
-    <div className='flex gap-6'>
-        <button>BROWSE COLLECTION</button>
-        <button>MANAGE BOOKS</button>
-    </div>
-    </div>
+    <div className='relative overflow-hidden flex flex-col py-20 px-8 gap-7 h-90'>
+
+  
+  <img
+    src='bg.png'
+    className='absolute inset-0 -z-10 w-full h-full object-cover'
+    alt='Background'
+  />
+
+ 
+  
+
+  <div className='flex flex-col gap-5'>
+    <h1 className='text-5xl text-white font-[Poppins] font-semibold'>
+      Discover Your Next Great Read
+    </h1>
+
+    <h3 className='text-xl font-[Poppins] text-white w-[50%]'>
+      Explore thousands of books across all genres. From timeless classics to the latest bestsellers.
+    </h3>
+  </div>
+
+  <div className='flex gap-6'>
+    <button>BROWSE COLLECTION</button>
+    <button>MANAGE BOOKS</button>
+  </div>
+</div>
     <Footer/>
     </>
 
